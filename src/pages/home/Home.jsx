89e459610527cd74c4951114e82868dc127c836f
Yagyesh <syagyesh91@gmail.com>
2023-05-05 10:17:25 +0530

    type animation added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React , {useEffect, useRef} from 'react'
 import Profile from "../../assets/home.jpg";
 import { Link } from 'react-router-dom';
 import {FaArrowRight} from "react-icons/fa";
 import "./home.css";
+import Typed from 'typed.js';
+
 
 const Home = () => {
+
+    const el = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ['Web-Developer','Front-End-Developer', 'Freelancer', 'UI Designer'],
+            typeSpeed: 60,
+            backSpeed: 100,
+            backDelay: 2000,
+        });
+
+        return () => {
+            typed.destroy();
+        };
+    }, []);
+   
   return (
     <section className="home section grid">
         <img src={Profile} alt="profile" className="home__img" />
@@ -12,7 +30,7 @@ const Home = () => {
         <div className="home__content">
             <div className="home__data">
                 <h1 className="home__title">
-                    <span>I'm Yagyesh Sharma,</span> Web Developer
+                    <span>I'm Yagyesh Sharma,</span> <span ref={el}></span>
                 </h1>
                 <p className="home__description">
                     I'm basically front-end web developer and learning backend to build my career.
