0298e957e2b7f4502d03344f4fb860accbd962f6
Yagyesh Sharma <syagyesh91@gmail.com>
2023-10-11 08:25:19 +0530

    resume updated and photo updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React , {useEffect, useRef} from 'react'
-import Profile from "../../assets/home.jpg";
+import Profile from "../../assets/userphoto.png";
 import { Link } from 'react-router-dom';
 import {FaArrowRight} from "react-icons/fa";
 import "./home.css";
